4ec5fdf0e53f5f1dd50548bb68ee724fd9edba5e
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-10 14:47:18 -0300

    Starting register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom"
-export default function LoginPage(){
+import { Link } from "react-router-dom";
+
+export default function RegisterPage(){
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
-            <h1 className="text-4xl text-center mb-4">Login</h1>
+            <h1 className="text-4xl text-center mb-4">Register</h1>
             <form className="max-w-md mx-auto">
                 <input type='email' placeholder="your@email.com"/>
+                <input type='text' placeholder="Jhon Doe"/>
                 <input type='password' placeholder="password"/>
                 <button className="primary">Login</button>
                 <div className="text-center py-2 text-gray-500">
-                Don't have an account yet? 
-                <Link className="underline text-black" to='/register'>Register now</Link>
+                Already a member? 
+                <Link className="underline text-black" to='/login'>Login</Link>
                 </div>
             </form>
             </div>
