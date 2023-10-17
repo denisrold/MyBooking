2dc7941c34ea3cad438cbeae0faa184185085579
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-17 19:56:03 -0300

    Refactoring and resolved bugs

---
+++ client/src/pages/PlacesFormPage.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import PhotosUploader from "../PhotosUploader";
+import Perks from "../Perks";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+
+export default function PlacesFormPage(){
+    const [title,setTitle]= useState('');
+    const [address,setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos]= useState([]);
+    const [description,setDescription] = useState('');
+    const [perks,setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkIn,setCheckIn] = useState('');
+    const [checkOut,setCheckOut] = useState('');
+    const [maxGuests,setMaxGuest]= useState(1);
+    
+    function inputHeader(text){
+        return (<h2 className="text-2xl mt-4">{text}</h2>)
+    };
+    function inputDescription(text){
+    return (
+        <p className="text-gray-500 text-sm">{text}</p>
+    )
+    };
+    function preInput(header,description){
+        return (
+            <>
+            {inputHeader(header)}
+            {inputDescription(description)}
+            </>
+        )
+    }
+    
+    async function addNewPLace(e){
+        e.preventDefault();
+    const {data} = await  axios.post('/places',{
+        address,title,addedPhotos,
+        description,perks,extraInfo,
+        checkIn,checkOut,maxGuests
+    });
+    setRedirectToPlacesList(true)
+    };
+
+
+    const [redirectToPlacesList,setRedirectToPlacesList] = useState(false);
+    
+    if(redirectToPlacesList  && !action !== 'new'){
+        return <Navigate to={'/account/places'}/>
+    }
+
+    return(
+<div>
+    <form onSubmit={addNewPLace}>
+        {preInput('Title','Title for you place, should be short and catchy as in advertisement')}
+        <input type='text' value={title} onChange={ev=>setTitle(ev.target.value)} placeholder="My lovely apt"/>
+        {preInput('Address','Address to this place.')}
+        <input type='text' value={address} onChange={ev=>setAddress(ev.target.value)} placeholder="address"/>
+        {preInput('Photos','More = better.')}
+        <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
+        {preInput('Description','Description of the place')}
+        <textarea value={description} onChange={ev=>setDescription(ev.target.value)}/>.
+        {preInput('Perks','Select all the perks of your place')}
+        <div className="grid mt-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
+            <Perks selected={perks} onChange={setPerks}/>
+        </div>
+        {preInput('Extra info','House rules, etc...')}
+        <textarea value={extraInfo} onChange={ev=>setExtraInfo(ev.target.value)}/> 
+        {preInput('Check in & out times, max guest','Add check in and out times, remember to have some time window for cleaning the room between guests')}
+        <div className="grid gap-2 sm:grid-cols-3 ">
+            <div>
+                <h3 className="mt-2 -mb-1">Check in time</h3>
+                <input value={checkIn} 
+                onChange={ev=>setCheckIn(ev.target.value)} 
+                type="number" 
+                placeholder="14" />
+            </div>
+            <div>
+                <h3 className="mt-2 -mb-1">Check out time</h3>
+                <input value={checkOut} 
+                onChange={ev=>setCheckOut(ev.target.value)} 
+                type="number" 
+                placeholder="12" />
+            </div>
+            <div>
+                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                <input value={maxGuests} 
+                onChange={ev=>setMaxGuest(ev.target.value)} 
+                type="number" 
+                placeholder="4" />
+            </div>
+        </div>
+        <button className="primary my-4">save</button>
+    </form>
+</div>
+    )
+    
+};
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,53 +1,20 @@
-import { useState } from "react";
-import { Link, Navigate, useParams } from "react-router-dom";
-import Perks from "../Perks";
-import axios from "axios";
-import PhotosUploader from "../PhotosUploader";
+import { Link, useParams } from "react-router-dom";
+import PlacesFormPage from "./PlacesFormPage";
 
 export default function PlacesPage(){
-    const [title,setTitle]= useState('');
-    const [address,setAddress] = useState('');
-    const [addedPhotos, setAddedPhotos]= useState([]);
-    const [description,setDescription] = useState('');
-    const [perks,setPerks] = useState([]);
-    const [extraInfo, setExtraInfo] = useState('');
-    const [checkIn,setCheckIn] = useState('');
-    const [checkOut,setCheckOut] = useState('');
-    const [maxGuests,setMaxGuest]= useState(1);
-    const [redirect,setRedirect] = useState('');
-    
     const {action} = useParams();
-    
-    function inputHeader(text){
-        return (<h2 className="text-2xl mt-4">{text}</h2>)
-    };
-    function inputDescription(text){
-    return (
-        <p className="text-gray-500 text-sm">{text}</p>
-    )
-    };
-    function preInput(header,description){
-        return (
-            <>
-            {inputHeader(header)}
-            {inputDescription(description)}
-            </>
-        )
-    }
-    
-    async function addNewPLace(e){
-        e.preventDefault();
-    const {data} = await  axios.post('/places',{
-        address,title,addedPhotos,
-        description,perks,extraInfo,
-        checkIn,checkOut,maxGuests
-    });
-    setRedirect('/account/places')
-    };
-
-    if(redirect){
-        return <Navigate to={redirect}/>
-    }
+    function linkClasses (type=null){
+        let classes = ' inline-flex gap-1 py-2 px-6 rounded-full'; 
+         if(type === false ){
+         classes += ' bg-primary text-white';
+         }
+         else
+         {
+             classes += ' bg-gray-200';
+         }
+         return classes;
+     }
+   
 return(
         <div>
         {action !== 'new' && (
@@ -61,49 +28,7 @@ return(
             </div>
             )}
         {action === 'new' && (
-            <div>
-                <form onSubmit={addNewPLace}>
-                    {preInput('Title','Title for you place, should be short and catchy as in advertisement')}
-                    <input type='text' value={title} onChange={ev=>setTitle(ev.target.value)} placeholder="My lovely apt"/>
-                    {preInput('Address','Address to this place.')}
-                    <input type='text' value={address} onChange={ev=>setAddress(ev.target.value)} placeholder="address"/>
-                    {preInput('Photos','More = better.')}
-                    <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
-                    {preInput('Description','Description of the place')}
-                    <textarea value={description} onChange={ev=>setDescription(ev.target.value)}/>.
-                    {preInput('Perks','Select all the perks of your place')}
-                    <div className="grid mt-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
-                        <Perks selected={perks} onChange={setPerks}/>
-                    </div>
-                    {preInput('Extra info','House rules, etc...')}
-                    <textarea value={extraInfo} onChange={ev=>setExtraInfo(ev.target.value)}/> 
-                    {preInput('Check in & out times, max guest','Add check in and out times, remember to have some time window for cleaning the room between guests')}
-                    <div className="grid gap-2 sm:grid-cols-3 ">
-                        <div>
-                            <h3 className="mt-2 -mb-1">Check in time</h3>
-                            <input value={checkIn} 
-                            onChange={ev=>setCheckIn(ev.target.value)} 
-                            type="number" 
-                            placeholder="14" />
-                        </div>
-                        <div>
-                            <h3 className="mt-2 -mb-1">Check out time</h3>
-                            <input value={checkOut} 
-                            onChange={ev=>setCheckOut(ev.target.value)} 
-                            type="number" 
-                            placeholder="12" />
-                        </div>
-                        <div>
-                            <h3 className="mt-2 -mb-1">Max number of guests</h3>
-                            <input value={maxGuests} 
-                            onChange={ev=>setMaxGuest(ev.target.value)} 
-                            type="number" 
-                            placeholder="4" />
-                        </div>
-                    </div>
-                    <button className="primary my-4">save</button>
-                </form>
-            </div>
+           <PlacesFormPage />
 )}
         myplaces
         </div>);
