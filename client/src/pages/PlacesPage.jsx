f03cf614d37a823fb92ded61d3eb22a57c02e02e
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-18 16:28:07 -0300

    Remove phtos and selected first photo

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -23,9 +23,9 @@ return(
             <div className="mt-4">
             {places.length > 0 && places.map(place=>(
                 <Link to={'/account/places/' + place._id} className="flex cursor-pointer gap-4 border p-4 rounded-2xl bg-gray-200" key={place._id}>
-                    <div className="bg-gray-300 w-32 h-32 ">{/*grow shrink-0*/}
+                    <div className=" flex bg-gray-300 w-32 h-32">{/*grow shrink-0*/}
                         {place.photos.length > 0 && (
-                            <img className="" src={'http://127.0.0.1:4000/uploads/'+place.photos[0]}/>
+                            <img className="object-cover" src={'http://127.0.0.1:4000/uploads/'+place.photos[0]}/>
                         )}
                     </div>
                     <div className="grow-0 shrink">
