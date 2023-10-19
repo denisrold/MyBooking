7b56c3bc911989ca99e982a0745f51d2a9b66afb
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-19 19:25:28 -0300

    photos and display photos finish

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -17,37 +17,50 @@ export default function PlacePage(){
 
     if(showAllPhotos){
         return(
-            <div className="absolute inset-0 bg-white  min-h-screen">
-                <div className="p-8 grid gap-4">
-                    <div>
-                        <h2 className="text-xl" >Photos of {place.title}</h2>
-                        <button className=" fixed flex right-10 top-20 gap-1 px-4 py-2 items-center rounded-2xl shadow shadow-gray-500">
+
+            <div className="absolute  inset-0  min-h-screen ">
+                <div className="p-8 grid gap-4  bg-black text-white">
+                    <div className="">
+                        <h2 className="text-xl text-center" >Photos of {place.title}</h2>
+                        <button 
+                        onClick={()=>{setShowAllPhotos(false)}}
+                        className="font-semibold fixed flex text-black bg-white top-20 gap-2 pr-5 pl-3 py-2 items-center rounded-2xl shadow-md shadow-gray-900"
+                        >
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                         <path fillRule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clipRule="evenodd" />
                         </svg>
 
-                            Close photos</button>
+                            Back</button>
                     </div>
-                <div className="p-8 grid gap-4">
+                <div className="p-8 grid gap-4 justify-center">
                 {place?.photos?.length > 0 && place?.photos?.map(photo=>(
-                    <img src={"http://127.0.0.1:4000/uploads/"+photo}></img>
+                    <img style={{'height':'500px'}} className=" rounded-md" src={"http://127.0.0.1:4000/uploads/"+photo}></img>
                 )) }
                 </div>
                 </div>
                
             </div>
+          
         )
     }
     return(
             <div className="mt-4 py-8 -mx-8 px-8 bg-gray-100">
                 <h1 className="text-2xl">{place.title}</h1>
+                
+                <div className="flex gap-1 items-center mb-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                </svg>
+
                 <a target="_blank" 
                 className="block underline font-semibold my-2" 
                 href={'https://maps.google.com/?q='+ place.address}>
                     {place.address}
                 </a>
+                </div>
                 <div className="relative">
-                    <div className="grid gap-2 grid-cols-[2fr_1fr]">
+                    <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
                         <div>
                             {place.photos?.[0] && (
                                 <div >
@@ -75,5 +88,6 @@ export default function PlacePage(){
                 </div>
                
             </div>
+        
     )
 };
