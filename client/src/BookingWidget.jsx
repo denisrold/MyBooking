07ccff5a23fd366b254ef1ca13a6bb3684cd95f4
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-20 16:11:01 -0300

    Starting save booking

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -5,6 +5,8 @@ export default function BookingWidget({place}){
     const [checkIn,setCheckIn] = useState('');
     const [checkout,setCheckOut] = useState('');
     const [numberOfGuests, setNumberOfGuests]= useState(1);
+    const [name,setName] = useState('');
+    const [mobile,setMobile] = useState('');
 
     let numberOfNight = 0;
 
@@ -33,6 +35,14 @@ export default function BookingWidget({place}){
                     <label>Number of guest: </label>
                     <input type='number' value={numberOfGuests} onChange={(e)=>{setNumberOfGuests(e.target.value)}} className="outline-none border-2 shadow-md" />
             </div>
+            {numberOfNight > 0 &&(
+                <div className="mb-2 border-gray-200 pt-2 px-4 border-t-2">
+                <label>Your full name: </label>
+                <input type='text' value={name} onChange={e=>{setName(e.target.value)}} className="outline-none border-2 shadow-md"  placeholder="James Smith"/>
+                <label>Phone number: </label>
+                <input type='tel' value={mobile} onChange={e=>{setMobile(e.target.value)}} className="outline-none border-2 shadow-md"  placeholder="xxxxx-xxxx-xxxx"/>
+            </div>
+            )}
         </div>
         <button className="primary" value={1}>Book this place
         {numberOfNight > 0 && (
