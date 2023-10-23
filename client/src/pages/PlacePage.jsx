05f1492e739b30fc77deb5c9c3086f4135fd2c90
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-10-23 20:27:02 -0300

    Extra info ok

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -26,8 +26,8 @@ export default function PlacePage(){
                 </div>
                 <PlaceGalery place={place} />
                 <div className=" my-8 gap-8 grid grid-cols-1 md:grid-cols-[2fr_1fr]">
-                    <div>
-                        <div className="my-4">
+                    <div className="lg:p-8">
+                    <div className="my-4">
                         <h2 className="font-semibold text-2xl">Description</h2>    
                         {place.description}
                     </div>
@@ -40,7 +40,7 @@ export default function PlacePage(){
                         <BookingWidget place={place}/>
                     </div>
                 </div>
-                <div className="bg-white -mx-8 px-8 pt-8 pb-1 my-4 border-t-2">
+                <div className="bg-white lg:px-16 -mx-8 px-8 pt-8 pb-1 my-4 border-t-2">
                     <h2 className="font-semibold text-2xl">Extrainfo: </h2>   
                     <div className="text-sm text-gray-800  mt-1 mb-4 leading-5" >{place.extraInfo}</div>
                 </div>
