9c304a4cd0d3530f612c8c706d75b2c2d0dc6850
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-01-17 16:45:18 -0300

    Responsive

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,8 @@ import { UserContext } from "./UserContext";
 export default function Header(){
   const {user} = useContext(UserContext);
    return( 
-   <header className=" flex justify-between items-center lg:px-16">
+    <>
+   <header className=" w-screen flex justify-evenly -ml-8  md:w-full md:flex  md:justify-between lg:px-16">
     <Link to='/' className="flex items-center gap-1">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -21,11 +22,11 @@ export default function Header(){
           d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
         />
       </svg>
-      <span className="font-bold text-xl">aircnc</span>
+      <span className=" font-bold text-xl">aircnc</span>
     </Link>
-    <div className="flex border lg:ml-12 border-gray-300 gap-2 rounded-full py-2 px-4 shadow-md shadow-gray-200">
+    <div className="hidden md:flex border lg:ml-12 border-gray-300 gap-2 rounded-full py-2 px-4 shadow-md shadow-gray-200">
       <div>Anywhere</div>
-      <div className="border-l border-gray-300"></div>
+      <div className=" border-l border-gray-300"></div>
       <div>Any week</div>
       <div className="border-l border-gray-300"></div>
       <div>Add guests</div>
@@ -76,8 +77,33 @@ export default function Header(){
         </svg>
       </div>
       {
-        !!user && (<div>{user.name}</div>)
+        !!user && (<div className="hidden md:block">{user.name}</div>)
       }
     </Link>
-  </header>)
+  </header>
+   <div className="flex md:hidden  mt-3 border w-fit mx-auto border-gray-300 gap-2 rounded-full py-2 px-4 shadow-md shadow-gray-200">
+   <div>Anywhere</div>
+   <div className=" border-l border-gray-300"></div>
+   <div>Any week</div>
+   <div className="border-l border-gray-300"></div>
+   <div className="hidden md:block">Add guests</div>
+   <button className=" bg-primary text-white p-1 rounded-full">
+     <svg
+       xmlns="http://www.w3.org/2000/svg"
+       fill="none"
+       viewBox="0 0 24 24"
+       strokeWidth={1.5}
+       stroke="currentColor"
+       className="w-4 h-4"
+     >
+       <path
+         strokeLinecap="round"
+         strokeLinejoin="round"
+         d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+       />
+     </svg>
+   </button>
+ </div>
+ </>)
+  
 };
