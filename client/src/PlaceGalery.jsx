cfde08ab4a6950c6098d671243508c312bd824ce
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-01-19 12:51:00 -0300

    button position

--- client/src/PlaceGalery.jsx
+++ client/src/PlaceGalery.jsx
@@ -52,7 +52,7 @@ export default function PlaceGalery({place}){
                 </div>
             </div>
             </div>
-        <button onClick={()=>{setShowAllPhotos(true)}} className="lg:-ml-16 flex gap-1 items-center absolute bottom-2 right-2 lg:w-64  lg:left-2/3 lg:right-2/3/4 py-2 px-4 rounded-2xl shadow-md shadow-gray-500 bg-white">
+        <button onClick={()=>{setShowAllPhotos(true)}} className=" flex gap-1 items-center absolute bottom-2 right-2 lg:w-64  lg:left-2/3 lg:right-2/3/4 py-2 px-4 rounded-2xl shadow-md shadow-gray-500 bg-white">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
         <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
         </svg>
